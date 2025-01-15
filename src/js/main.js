e1d1adc2dc65f16edda130256e35c70dc41e79a4
IdoiaBE <1992ibe@gmail.com>
2025-01-15 12:59:49 +0100

    APIs response received

--- src/js/main.js
+++ src/js/main.js
@@ -12,9 +12,22 @@ const searchBtn = document.querySelector('.js-searchBtn');
 
 const searchList = document.querySelector('.js-searchList')
 
+let resultData = [];
+
+function getDataAPI(seriesName){
+    fetch(`https://api.jikan.moe/v4/anime?q=${seriesName}`)
+    .then ((resp) => resp.json())
+    .then ((data) =>{
+        // console.log(data);
+        
+    });
+}
+
 const handleSearch = () => {
     let seriesName = searchText.value;
-    console.log(seriesName);
+    // console.log(seriesName);
+
+    getDataAPI(seriesName);
 }
 
 searchBtn.addEventListener('click', handleSearch)
