34733378bbd50c539859b9de29d02dd5975fce1b
IdoiaBE <1992ibe@gmail.com>
2025-01-15 12:27:52 +0100

    search value received

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('>> Ready :)');
+/* <li>
+        <img src="" alt="">
+        <h3>title</h3>
+    </li> 
+*/
+
+const searchText = document.querySelector('.js-searchText')
+
+const searchBtn = document.querySelector('.js-searchBtn');
+
+const searchList = document.querySelector('.js-searchList')
+
+const handleSearch = () => {
+    let seriesName = searchText.value;
+    console.log(seriesName);
+}
+
+searchBtn.addEventListener('click', handleSearch)
