2702abc52cc1606a526fa78d56f0b57e949a0a8c
IdoiaBE <1992ibe@gmail.com>
2025-01-15 15:13:36 +0100

    APIs response to result array

--- src/js/main.js
+++ src/js/main.js
@@ -12,22 +12,32 @@ const searchBtn = document.querySelector('.js-searchBtn');
 
 const searchList = document.querySelector('.js-searchList')
 
+//creo array donde voy a guardar los datos que me da la API (para luego pintarlos)
 let resultData = [];
 
+function selectInfo (){
+    for (const element of resultData){
+        let imgURL = element.images.jpg.image_url;
+        console.log(imgURL);
+    }
+}
+
 function getDataAPI(seriesName){
-    fetch(`https://api.jikan.moe/v4/anime?q=${seriesName}`)
+    fetch(`https://api.jikan.moe/v4/anime?q=${seriesName}`) //COMILLAS FRANCESAS PARA CONCATENAR
     .then ((resp) => resp.json())
-    .then ((data) =>{
-        // console.log(data);
-        
+    .then ((allData) =>{
+        console.log(allData);
+        resultData = allData.data; //primero lleno mi array con los datos
+
+
     });
 }
 
 const handleSearch = () => {
     let seriesName = searchText.value;
-    // console.log(seriesName);
+    //console.log(seriesName);
 
-    getDataAPI(seriesName);
+    getDataAPI(seriesName); //le paso el parámetro para luego poder utilizar este valor en la url de la API
 }
 
 searchBtn.addEventListener('click', handleSearch)
