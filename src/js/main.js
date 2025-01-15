5b694e7060ec2d5e2c88907176703789da827b3c
IdoiaBE <1992ibe@gmail.com>
2025-01-15 18:58:08 +0100

    APIs response painted in html

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,41 @@
 'use strict';
 
-/* <li>
-        <img src="" alt="">
-        <h3>title</h3>
-    </li> 
-*/
-
-const searchText = document.querySelector('.js-searchText')
+const searchText = document.querySelector('.js-searchText');
 
 const searchBtn = document.querySelector('.js-searchBtn');
 
-const searchList = document.querySelector('.js-searchList')
+const searchList = document.querySelector('.js-searchList');
 
-//creo array donde voy a guardar los datos que me da la API (para luego pintarlos)
+//creo un array donde voy a guardar los datos que me da la API (para luego pintarlos)
 let resultData = [];
 
+function renderSeries (imgURL, defaultTitle){
+    //si el elemento tiene los objetos que contienen la url, es decir, si tiene imagen
+    if (imgURL){
+        //pinto los datos recogidos
+        searchList.innerHTML += `<li>
+        <img src="${imgURL}" alt="${defaultTitle}">
+        <h3>${defaultTitle}</h3>
+        </li> `
+        } else{
+        searchList.innerHTML += `<li>
+        <img src='https://placehold.co/300x400?text=${defaultTitle}' alt='${defaultTitle}'>
+        <h3>${defaultTitle}</h3>
+        </li> `
+        }
+}
+
 function selectInfo (){
+    searchList.innerHTML = ''; //para limpiar la lista antes de volver a buscar resultados (porque el render se ejecuta dentro del bucle asi que esto tiene que estar fuera)
+
+    //primero hago un bucle para recorrer mi array
     for (const element of resultData){
-        let imgURL = element.images.jpg.image_url;
-        console.log(imgURL);
+        let imgURL = element.images.jpg.image_url; //recogo la url del objeto jpg, que a su vez está dentro del objeto images
+
+        let defaultTitle = element.titles.find(object => object.type === "Default")?.title; //como el title está dentro de otro array llamado titles, hay que recorrerlo. Usando find encuentro el objeto cuyo type es default para luego coger el title de ese objeto
+
+        renderSeries(imgURL, defaultTitle);
+       
     }
 }
 
@@ -26,16 +43,14 @@ function getDataAPI(seriesName){
     fetch(`https://api.jikan.moe/v4/anime?q=${seriesName}`) //COMILLAS FRANCESAS PARA CONCATENAR
     .then ((resp) => resp.json())
     .then ((allData) =>{
-        console.log(allData);
         resultData = allData.data; //primero lleno mi array con los datos
-
-
+        // console.log(resultData);
+        selectInfo();
     });
 }
 
 const handleSearch = () => {
     let seriesName = searchText.value;
-    //console.log(seriesName);
 
     getDataAPI(seriesName); //le paso el parámetro para luego poder utilizar este valor en la url de la API
 }
