e8803222c2339d2d4096f3f4bda1a474d230a6e6
IdoiaBE <1992ibe@gmail.com>
2025-01-16 09:37:06 +0100

    id saved when clicked

--- src/js/1_search.js
+++ src/js/1_search.js
@@ -4,9 +4,37 @@ const searchBtn = document.querySelector('.js-searchBtn');
 
 const searchList = document.querySelector('.js-searchList');
 
+const favList = document.querySelector('.js-favList');
+
 //creo un array donde voy a guardar los datos que me da la API (para luego pintarlos) y otro para almacenar los favoritos
 let resultData = [];
 let favouritesList = [];
+
+const renderFavourites = (selectedLi) => {
+    //recogo la url, el título y el id del li seleccionado
+    const imgURL = selectedLi.querySelector('img').src;
+    const defaultTitle = selectedLi.querySelector('h3').textContent;
+    const seriesId = selectedLi.id;
+
+    //lo pinto en la ul de favoritos
+    renderSeries(imgURL, defaultTitle, seriesId, favList);
+}
+
+function favouritesArrayManagement (selectedLi, selectedId){
+    if (selectedLi.classList.contains('bookmark')) {
+        // Si tiene la clase
+        if (!favouritesList.includes(selectedLi)) {
+            favouritesList.push(selectedLi); //y si el array de favoritos no incluye el li seleccionado, lo añado al array
+           
+            //y lo pinto en la ul
+            // renderFavourites();
+        }
+    } else {
+        // Si no tiene la clase, si se deselecciona
+        favouritesList = favouritesList.filter(item => item !== selectedLi);//filtro el array para que estén sólo los items que sean diferentes al li seleccionado
+    }
+    // console.log(favouritesList);
+}
 
 const selectFavourties = () => {
     const allLi = document.querySelectorAll('.js-listItem'); //Me devuelve un array
@@ -15,32 +43,24 @@ const selectFavourties = () => {
         eachLi.addEventListener('click', (ev)=>{
             let selectedLi = ev.currentTarget;
             selectedLi.classList.toggle('bookmark'); //le añado la clase de favorito
-            
-            if (selectedLi.classList.contains('bookmark')) {
-                // Si tiene la clase, lo añado al array
-                if (!favouritesList.includes(selectedLi)) {
-                    favouritesList.push(selectedLi); //si el array no incluye el li seleccionado, lo añado
-                }
-            } else {
-                // Si se deselecciona, lo elimino del array
-                favouritesList = favouritesList.filter(item => item !== selectedLi);//filtro el array para que estén sólo los items que sean diferentes al li seleccionado
-            }
-            // console.log(favouritesList);
+            let selectedId = ev.currentTarget.id; //cogo el id para buscarlo luego en el array de favoritos y pintarlo
+         
+            favouritesArrayManagement (selectedLi, selectedId);
         });
     }
 }
 
 
-function renderSeries (imgURL, defaultTitle, seriesId){
+function renderSeries (imgURL, defaultTitle, seriesId, list){
     //si el elemento tiene los objetos que contienen la url, es decir, si tiene imagen
     if (imgURL){
         //pinto los datos recogidos
-        searchList.innerHTML += `<li id="${seriesId}" class="js-listItem">
+        list.innerHTML += `<li id="${seriesId}" class="js-listItem">
         <img src="${imgURL}" alt="${defaultTitle}">
         <h3>${defaultTitle}</h3>
         </li> `
     } else{ //si no, le pongo un placeholder como img
-        searchList.innerHTML += `<li id="${seriesId}" class="js-listItem">
+        list.innerHTML += `<li id="${seriesId}" class="js-listItem">
         <img src="https://placehold.co/300x400?text=${defaultTitle}" alt="${defaultTitle}">
         <h3>${defaultTitle}</h3>
         </li> `
@@ -50,7 +70,8 @@ function renderSeries (imgURL, defaultTitle, seriesId){
 }
 
 function selectInfo (){
-    searchList.innerHTML = ''; //para limpiar la lista antes de volver a buscar resultados (porque el render se ejecuta dentro del bucle asi que esto tiene que estar fuera)
+    searchList.innerHTML = ''; //para limpiar la lista antes de volver a pintar otros resultados (porque el render se ejecuta dentro del bucle asi que esto tiene que estar fuera)
+
 
     //primero hago un bucle para recorrer mi array
     for (const element of resultData){
@@ -60,7 +81,7 @@ function selectInfo (){
 
         let seriesId = element.mal_id; //recogo el id de cada serie para luego poder hacer modificaciones
 
-        renderSeries(imgURL, defaultTitle, seriesId);
+        renderSeries(imgURL, defaultTitle, seriesId, searchList);
     }
 }
 
